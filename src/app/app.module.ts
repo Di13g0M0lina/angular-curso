83059eb0fd3ee4e11e6d6d8c245b7839e38f1a0a
Diego <friocolanalistafuncional@outlook.com>
2021-10-08 13:17:19 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorModule } from './Contador/Contador.module';
+
+import { HeroesModule } from './Heroes/Heroes.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
